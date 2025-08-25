a911379326f1ea9cbbddf99380819af7bfe0e840
kumi23114 <164824546+kumi23114@users.noreply.github.com>
2025-08-25 15:35:05 +0800

    Revert "文章顯示優化Vite"
    This reverts commit 7e315448dc43313f98dccd91e62e261bd8078328.

--- src/article-renderer.js
+++
@@ -1,7 +0,0 @@
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
-
-// 將 Contentful 渲染器掛到全域，供 article.html 既有邏輯使用
-window.documentToHtmlString = documentToHtmlString;
-console.log('✅ Contentful documentToHtmlString 已透過 Vite 掛載至 window');
-
-
